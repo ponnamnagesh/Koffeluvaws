f51abee36b25d0acebba585aa41335aa52bdcaab
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-30 12:07:02 -0500

    Create subnets.tf

---
+++ subnets.tf
@@ -0,0 +1,12 @@
+resource "aws_subnet" "publicA" {
+ #count      = length(var.public_subnet_cidrs)
+ vpc_id     = aws_vpc.main.id
+ #cidr_block = element(var.public_subnet_cidrs, count.index)
+  type        = list(string)
+ description = "Public Subnet CIDR values"
+  default     = ["172.16.1.0/24"]
+ 
+ tags = {
+   Name = "Public Subnet - publicA}"
+ }
+}
