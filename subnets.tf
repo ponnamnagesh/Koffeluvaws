b760ef62aac93e4732171526b9f60abb33e94bc0
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-30 12:12:39 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,9 +1,5 @@
 resource "aws_subnet" "publicA" {
- #count      = length(var.public_subnet_cidrs)
  vpc_id     = aws_vpc.main.id
- #cidr_block = element(var.public_subnet_cidrs, count.index)
-  #type        = list(string)
- #description = "Public Subnet CIDR values"
 cidr_block   = "172.16.1.0/24"
  
  tags = {
