ab32fd365858de12a8dcfe6d73cd267e117a09e8
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-30 12:19:46 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,8 +1,11 @@
 resource "aws_subnet" "publicA" {
  vpc_id     = aws_vpc.main.id
 cidr_block   = "172.16.1.0/24"
+ availability_zones      = "us-east-1a"
  
  tags = {
-   Name = "Public Subnet - publicA}"
+   Name = "Public Subnet - publicA"
  }
 }
+
+
