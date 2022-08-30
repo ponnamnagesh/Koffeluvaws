61a5eb8c4debc162d8e95c043fe0637fb50be41a
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-30 12:36:11 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "publicA" {
+resource "aws_subnet" "public_subnets" {
 vpc_id     = aws_vpc.main.id
 cidr_block   = "172.16.1.0/24"
 availability_zone      = "us-east-1a"
@@ -8,7 +8,7 @@ availability_zone      = "us-east-1a"
  }
 }
 
-resource "aws_subnet" "publicB" {
+resource "aws_subnet" "public_subnets" {
 vpc_id     = aws_vpc.main.id
 cidr_block   = "172.16.2.0/24"
 availability_zone      = "us-east-1b"
@@ -18,7 +18,7 @@ availability_zone      = "us-east-1b"
  }
 }
 
-resource "aws_subnet" "publicC" {
+resource "aws_subnet" "public_subnets" {
 vpc_id     = aws_vpc.main.id
 cidr_block   = "172.16.3.0/24"
 availability_zone      = "us-east-1c"
