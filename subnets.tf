54da5dcdb91f2e967b38b27b58a55e1dd71a6934
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-30 13:53:54 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -27,3 +27,67 @@ availability_zone      = "us-east-2c"
    Name = "Public Subnet - publicC"
  }
 }
+
+#Private subnets
+
+resource "aws_subnet" "privateAppA" {
+vpc_id     = aws_vpc.main.id
+cidr_block   = "172.16.4.0/24"
+availability_zone      = "us-east-2a"
+ 
+ tags = {
+   Name = "Private subnet - AppA"
+ }
+}
+
+resource "aws_subnet" "privateAppB" {
+vpc_id     = aws_vpc.main.id
+cidr_block   = "172.16.5.0/24"
+availability_zone      = "us-east-2b"
+ 
+ tags = {
+   Name = "Private subnet - AppB"
+ }
+}
+
+resource "aws_subnet" "privateAppC" {
+vpc_id     = aws_vpc.main.id
+cidr_block   = "172.16.6.0/24"
+availability_zone      = "us-east-2c"
+ 
+ tags = {
+   Name = "Private subnet - AppB"
+ }
+}
+
+# Private Subnet DB
+
+resource "aws_subnet" "privateDbA" {
+vpc_id     = aws_vpc.main.id
+cidr_block   = "172.16.8.0/24"
+availability_zone      = "us-east-2a"
+ 
+ tags = {
+   Name = "Private subnet - DbA"
+ }
+}
+
+resource "aws_subnet" "privateDbB" {
+vpc_id     = aws_vpc.main.id
+cidr_block   = "172.16.9.0/24"
+availability_zone      = "us-east-2b"
+ 
+ tags = {
+   Name = "Private subnet - DbB"
+ }
+}
+
+resource "aws_subnet" "privateDbC" {
+vpc_id     = aws_vpc.main.id
+cidr_block   = "172.16.10.0/24"
+availability_zone      = "us-east-2c"
+ 
+ tags = {
+   Name = "Private subnet - DbC"
+ }
+}
