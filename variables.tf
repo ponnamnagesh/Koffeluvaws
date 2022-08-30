cbb83a464e8e9864c6270743cf6f4a5641a75f6a
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-30 16:09:39 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,14 @@ variable "region" {
   default     = "us-east-1"
   type        = string
 }
+
+variable "namespace" {
+  description = "The project namespace to use for unique resource naming"
+  type        = string
+}
+
+variable "ssh_keypair" {
+  description = "SSH keypair to use for EC2 instance"
+  default     = null
+  type        = string
+}
