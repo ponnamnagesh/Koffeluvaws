e956c12582fc2261645d49d51dc7e550348ec5cf
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-08-31 20:47:38 -0500

    Update eip.tf

--- eip.tf
+++ eip.tf
@@ -1,10 +1,10 @@
-resource "aws_eip" "one" {
+resource "aws_eip" "nat1" {
   vpc                       = true
   network_interface         = aws_network_interface.multi-ip.id
   associate_with_private_ip = "10.0.0.10"
 }
 
-resource "aws_eip" "two" {
+resource "aws_eip" "nat2" {
   vpc                       = true
   network_interface         = aws_network_interface.multi-ip.id
   associate_with_private_ip = "10.0.0.11"
