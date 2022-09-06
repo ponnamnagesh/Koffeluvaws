793bd63c570938871d134d64eb87bb4d14ef82b4
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-06 10:09:00 -0500

    added new strategic files and arranged folders

---
+++ modules/compute/variables.tf
@@ -0,0 +1,36 @@
+variable "project_name" {
+  type        = string
+  default     = "liveproject_TAWS"
+  description = "Name for the live_project"
+}
+
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "Region to deploy the Stack in"
+}
+
+variable "AppSubnet_IDs" {
+  type        = map(string)
+  
+  description = "Map of App Subnet IDs"
+}
+
+variable "PublicSubnet_IDs" {
+  type        = map(string)
+  
+  description = "Map of Public Subnet IDs"
+}
+
+variable "key_name" {
+  type        = string
+  description = "KeyName for precreated SSH Key in AWS"
+}
+variable "BastionSG" {
+  type        = string
+  description = "Sec Group Bastions Hosts"
+}
+variable "AppSG" {
+  type        = string
+  description = "Sec Group App Servers"
+}
