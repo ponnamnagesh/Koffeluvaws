9088b65aa25c762d91e2ea15e0d1dab92715102f
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-06 10:47:01 -0500

    deleted compute security creating diff branch

--- main.tf
+++ main.tf
@@ -4,21 +4,3 @@ module "network" {
   region = var.region
 }
 
-module "compute" {
-  source    = "./modules/compute"
-  #namespace = var.namespace
-  #region = var.region
-}
-
-module "security" {
-  source    = "./modules/security"
-  #namespace = var.namespace
-  region = var.region
-  
-  #project_name     = var.project_name
-  #PublicSubnet_IDs = module.network.PublicSubnet_IDs
-  #AppSubnet_IDs    = module.network.AppSubnet_IDs
-  #key_name         = var.key_name
-  #BastionSG        = module.security.BastionSG
-  #AppSG            = module.security.AppSG
-}
