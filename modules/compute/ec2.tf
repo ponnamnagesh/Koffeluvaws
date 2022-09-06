bcbb59702e73c572946a6a4e0fb53806996cb8e9
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-05 23:53:59 -0500

    Update ec2.tf

---
+++ modules/compute/ec2.tf
@@ -0,0 +1,26 @@
+// Choose latest Amazon2 Linux AMI for x86_64 
+
+data "aws_ami" "amaz_linux" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+filter {
+    name   = "name"
+    values = ["*amzn2-*"]
+  }
+  
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
