c9060dee5f574ed82dfeea5450b8ffd409ad1977
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-06 10:45:44 -0500

    corrected

--- main.tf
+++ main.tf
@@ -6,19 +6,19 @@ module "network" {
 
 module "compute" {
   source    = "./modules/compute"
-  namespace = var.namespace
-  region = var.region
+  #namespace = var.namespace
+  #region = var.region
 }
 
 module "security" {
   source    = "./modules/security"
-  namespace = var.namespace
+  #namespace = var.namespace
   region = var.region
   
   #project_name     = var.project_name
-  PublicSubnet_IDs = module.network.PublicSubnet_IDs
-  AppSubnet_IDs    = module.network.AppSubnet_IDs
-  key_name         = var.key_name
-  BastionSG        = module.security.BastionSG
-  AppSG            = module.security.AppSG
+  #PublicSubnet_IDs = module.network.PublicSubnet_IDs
+  #AppSubnet_IDs    = module.network.AppSubnet_IDs
+  #key_name         = var.key_name
+  #BastionSG        = module.security.BastionSG
+  #AppSG            = module.security.AppSG
 }
