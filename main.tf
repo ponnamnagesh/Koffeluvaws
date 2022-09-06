1aadb22b7a166fb25864cdc66294c7f14457bd41
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-06 10:21:10 -0500

    added to folder structure

--- main.tf
+++ main.tf
@@ -1,17 +1,23 @@
 module "network" {
-  source    = "./modules/network"
-  namespace = var.namespace
-  region = var.region
-}
-
-module "compute" {
-  source    = "./modules/compute"
-  namespace = var.namespace
-  region = var.region
+    source = ".modules/network"
+    region = var.region
+    project_name = var.project_name
 }
 
 module "security" {
-  source    = "./modules/security"
-  namespace = var.namespace
-  region = var.region
+    source = ".modules/security"
+    vpc_id = module.modules.network.vpc_id
+    region           = var.region
+    project_name     = var.project_name
+}
+
+module "compute" {
+    source           = ".modules/compute"
+    region           = var.region
+    project_name     = var.project_name
+    PublicSubnet_IDs = module.modules.network.PublicSubnet_IDs
+    AppSubnet_IDs    = module.modules.network.AppSubnet_IDs
+    key_name         = var.key_name
+    BastionSG        = module.modules.security.BastionSG
+    AppSG            = module.modules.security.AppSG
 }
--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,65 +1,68 @@
-variable "namespace" {
-   type = string
+variable "project_name" {
+  type        = string
+  default     = "liveproject_TAWS"
+  description = "Name for the live_project"
 }
 
 variable "region" {
-   type = string
+  type        = string
+  default     = "us-east-1"
+  description = "Region to deploy the Stack in"
 }
 
-variable "vpc_cidr_block" {
-   type = string
-   default = "172.16.0.0/16"
+variable "publicsubnets" {
+  type = object({
+
+    subnet1 = list(string),
+    subnet2 = list(string),
+    subnet3 = list(string)
+  })
+
+  default = {
+    subnet1 = ["publicA", "172.16.1.0/24", "us-east-1a"],
+    subnet2 = ["publicB", "172.16.2.0/24", "us-east-1b"],
+    subnet3 = ["publicC", "172.16.3.0/24", "us-east-1c"]
+  }
+  description = "CIDRs for the public subnets, note that the nr of subnets determines the nr. of EIPs and NAT_GWs as well"
+}
+variable "vpc_cidr" {
+  type        = string
+  default     = "172.16.0.0/16"
+  description = "CIDR Range to use for the VPC"
 }
 
-variable "public_subnet_cidr_blocks" {
-  description = "Available cidr blocks for public subnets"
-  type        = list(string)
-  default = [
-     "172.16.1.0/24",
-     "172.16.2.0/24",
-     "172.16.3.0/24",
-  ]
+variable "dbsubnets" {
+  type = object({
+
+    subnet1 = list(string),
+    subnet2 = list(string),
+    subnet3 = list(string)
+  })
+
+  default = {
+    subnet1 = ["DbA", "172.16.8.0/24", "us-east-1a"],
+    subnet2 = ["DbB", "172.16.9.0/24", "us-east-1b"],
+    subnet3 = ["DbC", "172.16.10.0/24", "us-east-1c"]
+  }
+  description = "CIDRs for the public subnets, note that the nr of subnets determines the nr. of EIPs and NAT_GWs as well"
+}
+
+variable "appsubnets" {
+  type = object({
+
+    subnet1 = list(string),
+    subnet2 = list(string),
+    subnet3 = list(string)
+  })
+
+  default = {
+    subnet1 = ["AppA", "172.16.4.0/24", "us-east-1a"],
+    subnet2 = ["AppB", "172.16.5.0/24", "us-east-1b"],
+    subnet3 = ["AppC", "172.16.6.0/24", "us-east-1c"]
+  }
+  description = "Infos for the Appsubnets, note that the nr of subnets determines the nr. of EIPs and NAT_GWs as well"
 }
 
 
-variable "private_subnet_cidr_blocks" {
-  description = "Available cidr blocks for private subnets"
-  type        = list(string)
-  default = [
-     "172.16.4.0/24",
-     "172.16.5.0/24",
-     "172.16.6.0/24"
-  ]
-}
 
 
-variable "database_subnet_cidr_blocks" {
-  description = "Available cidr blocks for database subnets"
-  type        = list(string)
-  default = [
-     "172.16.7.0/24",
-     "172.16.8.0/24",
-     "172.16.9.0/24"
-  ]
-}
-
-
-variable "azs" {
-   type = list(string)
-   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "az_tag" {
-   type = list(string)
-   default = [
-      "PublicA",
-      "publicB",
-      "publicC",
-      "AppA",
-      "AppB",
-      "AppC",
-      "DbA",
-      "DbB",
-      "DbC"
-   ]
-}
