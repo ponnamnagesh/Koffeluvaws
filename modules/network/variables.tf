8f46e49fff6aea230ef257ab40c52d98d9ac5e10
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-08 10:47:34 -0500

    Update variables.tf

--- modules/network/variables.tf
+++ modules/network/variables.tf
@@ -1,68 +1,64 @@
-variable "project_name" {
-  type        = string
-  default     = "liveproject_TAWS"
-  description = "Name for the live_project"
+variable "vpc_cidr_block" {
+    type = string
+    default = "172.16.0.0/16"
 }
 
-variable "region" {
-  type        = string
-  default     = "us-east-1"
-  description = "Region to deploy the Stack in"
+variable "publicA_subnet_cidr_block" {
+    type = string
+    default = "172.16.1.0/24"
 }
 
-variable "publicsubnets" {
-  type = object({
-
-    subnet1 = list(string),
-    subnet2 = list(string),
-    subnet3 = list(string)
-  })
-
-  default = {
-    subnet1 = ["publicA", "172.16.1.0/24", "us-east-1a"],
-    subnet2 = ["publicB", "172.16.2.0/24", "us-east-1b"],
-    subnet3 = ["publicC", "172.16.3.0/24", "us-east-1c"]
-  }
-  description = "CIDRs for the public subnets, note that the nr of subnets determines the nr. of EIPs and NAT_GWs as well"
-}
-variable "vpc_cidr" {
-  type        = string
-  default     = "172.16.0.0/16"
-  description = "CIDR Range to use for the VPC"
+variable "publicB_subnet_cidr_block" {
+    type = string
+    default = "172.16.2.0/24"
 }
 
-variable "dbsubnets" {
-  type = object({
-
-    subnet1 = list(string),
-    subnet2 = list(string),
-    subnet3 = list(string)
-  })
-
-  default = {
-    subnet1 = ["DbA", "172.16.8.0/24", "us-east-1a"],
-    subnet2 = ["DbB", "172.16.9.0/24", "us-east-1b"],
-    subnet3 = ["DbC", "172.16.10.0/24", "us-east-1c"]
-  }
-  description = "CIDRs for the public subnets, note that the nr of subnets determines the nr. of EIPs and NAT_GWs as well"
+variable "publicC_subnet_cidr_block" {
+    type = string
+    default = "172.16.3.0/24"
 }
 
-variable "appsubnets" {
-  type = object({
-
-    subnet1 = list(string),
-    subnet2 = list(string),
-    subnet3 = list(string)
-  })
-
-  default = {
-    subnet1 = ["AppA", "172.16.4.0/24", "us-east-1a"],
-    subnet2 = ["AppB", "172.16.5.0/24", "us-east-1b"],
-    subnet3 = ["AppC", "172.16.6.0/24", "us-east-1c"]
-  }
-  description = "Infos for the Appsubnets, note that the nr of subnets determines the nr. of EIPs and NAT_GWs as well"
+variable "appA_subnet_cidr_block" {
+    type = string
+    default = "172.16.4.0/24"
 }
 
+variable "appB_subnet_cidr_block" {
+    type = string
+    default = "172.16.5.0/24"
+}
 
+variable "appC_subnet_cidr_block" {
+    type = string
+    default = "172.16.6.0/24"
+}
 
+variable "dbA_subnet_cidr_block" {
+    type = string
+    default = "172.16.8.0/24"
+}
 
+variable "dbB_subnet_cidr_block" {
+    type = string
+    default = "172.16.9.0/24"
+}
+
+variable "dbC_subnet_cidr_block" {
+    type = string
+    default = "172.16.10.0/24"
+}
+
+variable "az1a" {
+    type = string
+    default = "us-east-1a"
+}
+
+variable "az1b" {
+    type = string
+    default = "us-east-1b"
+}
+
+variable "az1c" {
+    type = string
+    default = "us-east-1c"
+}
