2d3ed5b5c909bf38723708b068eb5ae0143bb8c9
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-08 10:44:48 -0500

    Update variables.tf

--- modules/containers/variables.tf
+++ modules/containers/variables.tf
@@ -1,9 +1,26 @@
 variable "myLabKeyPair" {
- type = string
- default     = "kluvkey"
+    type = string
 }
 
+variable "EcsSG" {
+    type = string
+}
 
+variable "appA" {
+    type = string
+}
+
+variable "appB" {
+    type = string
+}
+
+variable "appC" {
+    type = string
+}
+
+variable "ecsInstanceProfileId" {
+    type = string
+}
 
 // variable "ecsExecutionRoleArn" {
 // 	type = string
