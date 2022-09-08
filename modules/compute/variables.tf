7c51948b367e8a153bcafaa8a21c856de2f20e8c
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-08 11:52:22 -0500

    Update variables.tf

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -1,36 +1,39 @@
-variable "key_pair_name" {
-    type = string
-    default = "myLabKeyPair"
+variable "project_name" {
+  type        = string
+  default     = "liveproject_TAWS"
+  description = "Name for the live_project"
 }
 
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "Region to deploy the Stack in"
+}
+
+variable "PublicSubnet_IDs" {
+  type        = map(string)
+  
+  description = "Map of Public Subnet IDs"
+}
+
+variable "key_name" {
+  type        = string
+  description = "KeyName for precreated SSH Key in AWS"
+}
 variable "BastionSG" {
-    type = string
+  type        = string
+  description = "Sec Group Bastions Hosts"
 }
 
-variable "publicSubnetA" {
-    type = string
-}
 
-variable "publicSubnetB" {
-    type = string
-}
 
-variable "publicSubnetC" {
-    type = string
-}
 
-variable "AppSG" {
-    type = string
-}
 
-variable "appA" {
-    type = string
-}
 
-variable "appB" {
-    type = string
-}
 
-variable "appC" {
-    type = string
-}
+
+
+
+
+
+
