8b4ddf7f2a446314e2630da9ce188f8bbeb05dc0
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-08 10:49:36 -0500

    Update outputs.tf

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -1,7 +1,19 @@
-output "AppSG" {
-  value = aws_security_group.APPSG.id
+output "BastionSG" {
+    value = aws_security_group.BastionSG.id
+    description = "The generated security group id"
 }
 
-output "BastionSG" {
-  value = aws_security_group.BastionSG.id
+output "AppSG" {
+    value = aws_security_group.AppSG.id
+    description = "The generated security group id"
+}
+
+output "EcsSG" {
+    value = aws_security_group.EcsSG.id
+    description = "The generated security group id"
+}
+
+output "EcsInstanceProfileId" {
+    value = aws_iam_instance_profile.ecsInstanceProfile.id
+    description = "The generated security group id"
 }
