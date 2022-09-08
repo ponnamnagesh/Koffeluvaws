d7fe10493beef5fcbaaddcb08238a4225d2aff9a
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-08 10:50:49 -0500

    Update variables.tf

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -1,16 +1,3 @@
-variable "project_name" {
-  type        = string
-  default     = "liveproject_TAWS"
-  description = "Name for the live_project"
-}
-
-variable "region" {
-  type        = string
-  default     = "us-east-1"
-  description = "Region to deploy the Stack in"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC_ID"
+variable "vpcId" {
+    type = string
 }
