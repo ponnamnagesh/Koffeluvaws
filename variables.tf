cc1e6f72f9d3e6b944ba51a592f64fb5b7401dda
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-08 10:39:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,4 @@
-variable "project_name" {
-  type        = string
-  default     = "liveproject_TAWS"
-  description = "Name for the live_project"
-}
-
 variable "region" {
-  type        = string
-  default     = "us-east-1"
-  description = "Region to deploy the Stack in"
-}
-
-variable "key_name" {
-  type        = string
-  description = "KeyName for precreated SSH Key in AWS"
-  default     = "kluvkey"
+    type = string
+    default = "us-east-1"
 }
