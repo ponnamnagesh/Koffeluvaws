e451991ca66dff23aec8d079643c86bfab36bdfc
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-12 15:39:18 -0500

    Update iam.tf

--- modules/security/iam.tf
+++ modules/security/iam.tf
@@ -1,3 +1,5 @@
+# Could be removed if the EOF version down below works
+
 data "aws_iam_policy_document" "ecs_pol" {
   statement {
     actions = ["sts:AssumeRole"]
@@ -11,7 +13,23 @@ data "aws_iam_policy_document" "ecs_pol" {
 
 resource "aws_iam_role" "ecsInstancerole" {
   name               = "ecsInstanceRole"
-  assume_role_policy = data.aws_iam_policy_document.ecs_pol.json
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  
+#data.aws_iam_policy_document.ecs_pol.json
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_agent" {
@@ -22,4 +40,31 @@ resource "aws_iam_role_policy_attachment" "ecs_agent" {
 resource "aws_iam_instance_profile" "ecs_agent-profile" {
   name = "ecs-agent-profile"
   role = aws_iam_role.ecsInstancerole.name
+}
+
+## Task Execution role 
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "${var.project_name}-ecsTaskExecutionRole"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
