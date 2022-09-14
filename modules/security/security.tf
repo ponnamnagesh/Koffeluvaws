2258af614a6ffb2c44802d36acdbe7d97fd3af9f
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-13 22:34:09 -0500

    Update security.tf

--- modules/security/security.tf
+++ modules/security/security.tf
@@ -26,8 +26,8 @@ resource "aws_security_group" "BastionSG" {
 }
 
 resource "aws_security_group" "ECSSG" {
-  name        = "Allow_ssh_for_bastions_ECS"
-  description = "Allow SSH Inbound from Bastion Hosts"
+  name        = "Allow Access from Bastion & ALB to ECS Instances"
+  description = "Allow Access from Bastion & ALB to ECS Instances"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -38,7 +38,66 @@ resource "aws_security_group" "ECSSG" {
     security_groups=[aws_security_group.BastionSG.id]
     
   }
+  
+  ingress {
+    description      = "HTTP from ALB"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    security_groups=[aws_security_group.ALBSG.id]
+    
+  }
+   ingress {
+    description      = "Ephemeral from ALB"
+    from_port        = 32768
+    to_port          = 65535
+    protocol         = "tcp"
+    security_groups=[aws_security_group.ALBSG.id]
+    
+  }
+   
+   ingress {
+    description      = "Containerport"
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    security_groups=[aws_security_group.ALBSG.id]
+    
+  }
+
+  egress {
+   protocol         = "-1"
+   from_port        = 0
+   to_port          = 0
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+
+
   tags = {
     Name = "Allow Ingress SSH only from Bastions"
+  }
+}
+
+resource "aws_security_group" "ALBSG" {
+  name   = "${var.project_name}-sg-alb"
+  vpc_id = var.vpc_id
+ 
+  ingress {
+   protocol         = "tcp"
+   from_port        = 80
+   to_port          = 80
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+   protocol         = "-1"
+   from_port        = 0
+   to_port          = 0
+   cidr_blocks      = ["0.0.0.0/0"]
+   ipv6_cidr_blocks = ["::/0"]
   }
 }
