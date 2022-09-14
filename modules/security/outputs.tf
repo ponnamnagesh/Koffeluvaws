efdafc481a421122e46f87920c911645cce5a373
ponnamnagesh <74391064+ponnamnagesh@users.noreply.github.com>
2022-09-13 22:00:41 -0500

    Update outputs.tf

--- modules/security/outputs.tf
+++ modules/security/outputs.tf
@@ -9,3 +9,10 @@ output "ECSSG" {
 output "ECSProfile" {
   value = aws_iam_instance_profile.ecs_agent-profile.id
 }
+output "ecsTaskExecutionRolearn" {
+  value = aws_iam_role.ecs_task_execution_role.arn
+}
+
+output "ALBSG" {
+  value = aws_security_group.ALBSG.id
+}
